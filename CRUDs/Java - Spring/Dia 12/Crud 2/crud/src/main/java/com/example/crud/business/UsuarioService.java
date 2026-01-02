a2376ccc1d0b11a414f520580b961e1732a8742d
diogo <alvarwngadiogo228@gmail.com>
2026-01-02 20:45:40 -0300

    commit

---
+++ CRUDs/Java - Spring/Dia 12/Crud 2/crud/src/main/java/com/example/crud/business/UsuarioService.java
@@ -0,0 +1,53 @@
+package com.example.crud.business;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.crud.DTO.UsuarioDTO;
+import com.example.crud.infrastructure.entity.Usuario;
+import com.example.crud.infrastructure.repository.UsuarioRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+	
+	private final UsuarioRepository repository;
+
+	private final PasswordEncoder configSenha;
+	
+	public UsuarioDTO adicionarUsuario(Usuario usuario) {
+		repository.saveAndFlush(usuario);
+		
+		return new UsuarioDTO(usuario.getId(),usuario.getNome(),usuario.getEmail());
+	}
+	
+	public UsuarioDTO buscarUsuario(Long id) {
+		Usuario usuario = repository.findById(id).orElseThrow(
+				() -> new EntityNotFoundException("Id não encontrado"));
+		
+		return new UsuarioDTO(usuario.getId(),usuario.getNome(),usuario.getEmail());
+	}
+	
+	public void deletarUsuario(Long id) {
+		repository.deleteById(id);
+	}
+	
+	public UsuarioDTO atualizarUsuario(Usuario usuario, Long id) {
+		Usuario usuarioEntity = repository.findById(id).orElseThrow(
+				() -> new EntityNotFoundException("Id não encontrado"));
+		
+		Usuario usuarioAtualizado = Usuario.builder()
+				.nome(usuario.getNome()!= null? usuario.getNome(): usuarioEntity.getNome())
+				.email(usuario.getEmail()!= null? usuario.getEmail(): usuarioEntity.getEmail())
+				.id(usuarioEntity.getId())
+				.senha(usuario.getSenha()!= null? configSenha.encode(usuario.getSenha()): usuarioEntity.getSenha())
+				.build();
+		
+		repository.saveAndFlush(usuarioAtualizado);
+		
+		return new UsuarioDTO(usuarioAtualizado.getId(),usuarioAtualizado.getNome(),usuarioAtualizado.getEmail());
+	}
+}
---
+++ CRUDs/Java - Spring/Dia 12/Crud 2/crud/src/main/java/com/example/crud/infrastructure/entity/Usuario.java
@@ -0,0 +1,37 @@
+package com.example.crud.infrastructure.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Table(name = "usuario")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "senha")
+	private String senha;
+	
+}
