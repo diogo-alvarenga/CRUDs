dd5665078b65877cf6961bbfb2047383ce54852b
diogo <alvarwngadiogo228@gmail.com>
2026-01-11 16:33:59 -0300

    commit

---
+++ CRUDs/Java - Spring/Dia 21/crud/src/main/java/com/example/crud/infrastructure/entity/Usuario.java
@@ -0,0 +1,36 @@
+package com.example.crud.infrastructure.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuario")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "email", unique = true)
+	private String email;
+
+	@Column(name = "senha")
+	private String senha;
+}
