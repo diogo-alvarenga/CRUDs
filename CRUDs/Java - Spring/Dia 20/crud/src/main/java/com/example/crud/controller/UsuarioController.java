0ec2fe2b2a188a4c09e768dc8156926d13d5cbd5
diogo <alvarwngadiogo228@gmail.com>
2026-01-10 22:15:36 -0300

    commit

---
+++ CRUDs/Java - Spring/Dia 20/crud/src/main/java/com/example/crud/controller/UsuarioController.java
@@ -0,0 +1,48 @@
+package com.example.crud.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.crud.DTO.UsuarioDTO;
+import com.example.crud.business.UsuarioService;
+import com.example.crud.infrastructure.entity.Usuario;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/usuario")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+	private final UsuarioService service;
+
+	@PostMapping
+	public ResponseEntity<UsuarioDTO> adicionarUsuario(@RequestBody Usuario usuario){
+		return ResponseEntity.ok(service.adicionarUsuario(usuario));
+	}
+	
+	@PutMapping
+	public ResponseEntity<UsuarioDTO> atualizarUsuario(@RequestBody Usuario usuario, @RequestParam Long id){
+		return ResponseEntity.ok(service.atualizarUsuario(usuario, id));
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<Void> deletarUsuario(@RequestParam Long id){
+		service.deletarUsuario(id);
+		return ResponseEntity.ok().build();
+	}
+	
+	@GetMapping
+	public List<UsuarioDTO> listarTodos(){
+		return service.listarTodos();
+	}
+}
