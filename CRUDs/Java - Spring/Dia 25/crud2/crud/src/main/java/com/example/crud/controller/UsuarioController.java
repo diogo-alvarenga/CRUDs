aff5a230111257197cd9cda12eb2dce94fbc828c
diogo <alvarwngadiogo228@gmail.com>
2026-01-15 17:45:05 -0300

    commit

---
+++ CRUDs/Java - Spring/Dia 25/crud2/crud/src/main/java/com/example/crud/controller/UsuarioController.java
@@ -0,0 +1,87 @@
+package com.example.crud.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import java.util.List;
+
+import com.example.crud.DTO.LoginDTO;
+import com.example.crud.DTO.UsuarioDTO;
+import com.example.crud.business.UsuarioService;
+import com.example.crud.infrastructure.entity.Usuario;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/usuario")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+	private final UsuarioService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UsuarioDTO> buscarUsuario(@PathVariable Long id){
+		UsuarioDTO dto = service.buscarUsuario(id);
+		dto.add(linkTo(methodOn(UsuarioController.class).deletarUsuario(null)).withRel("deletar"));
+		dto.add(linkTo(methodOn(UsuarioController.class).adicionarUsuario(null)).withRel("adicionar"));
+		dto.add(linkTo(methodOn(UsuarioController.class).atualizarUsuario(null, null)).withRel("atualizar"));
+		dto.add(linkTo(methodOn(UsuarioController.class).listarTodos()).withRel("listar"));
+		dto.add(linkTo(methodOn(UsuarioController.class).buscarUsuario(null)).withSelfRel());
+		
+		return ResponseEntity.ok(dto);
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<Void> deletarUsuario(@RequestParam Long id){
+		service.deletarUsuario(id);
+		return ResponseEntity.ok().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<UsuarioDTO> adicionarUsuario(@RequestBody Usuario usuario){
+		UsuarioDTO dto = service.adicionarUsuario(usuario);
+		dto.add(linkTo(methodOn(UsuarioController.class).buscarUsuario(null)).withRel("buscar"));
+		dto.add(linkTo(methodOn(UsuarioController.class).deletarUsuario(null)).withRel("deletar"));
+		dto.add(linkTo(methodOn(UsuarioController.class).adicionarUsuario(null)).withSelfRel());
+		dto.add(linkTo(methodOn(UsuarioController.class).atualizarUsuario(null,null)).withRel("atualizar"));
+		dto.add(linkTo(methodOn(UsuarioController.class).listarTodos()).withRel("listar"));
+		return ResponseEntity.ok(dto);
+	}
+	
+	@PutMapping
+	public ResponseEntity<UsuarioDTO> atualizarUsuario(@RequestBody Usuario usuario, @RequestParam Long id){
+		UsuarioDTO dto = service.atualizarUsuario(usuario, id);
+		dto.add(linkTo(methodOn(UsuarioController.class).atualizarUsuario(null, null)).withSelfRel());
+		dto.add(linkTo(methodOn(UsuarioController.class).buscarUsuario(null)).withRel("buscar"));
+		dto.add(linkTo(methodOn(UsuarioController.class).deletarUsuario(null)).withRel("deletar"));
+		dto.add(linkTo(methodOn(UsuarioController.class).adicionarUsuario(null)).withRel("adicionar"));
+		dto.add(linkTo(methodOn(UsuarioController.class).listarTodos()).withRel("listar"));
+		return ResponseEntity.ok(dto);
+	}
+	
+	@PostMapping(value="/login")
+	public ResponseEntity<UsuarioDTO> login(@RequestBody LoginDTO login){
+		UsuarioDTO dto = service.login(login);
+		dto.add(linkTo(methodOn(UsuarioController.class).adicionarUsuario(null)).withRel("adicionar"));
+		dto.add(linkTo(methodOn(UsuarioController.class).buscarUsuario(null)).withRel("buscar"));
+		dto.add(linkTo(methodOn(UsuarioController.class).atualizarUsuario(null, null)).withRel("atualizar"));
+		dto.add(linkTo(methodOn(UsuarioController.class).listarTodos()).withRel("listar"));
+		dto.add(linkTo(methodOn(UsuarioController.class).deletarUsuario(null)).withRel("deletar"));
+		return ResponseEntity.ok(dto);
+	}
+	
+	@GetMapping
+	public List<UsuarioDTO> listarTodos(){
+		return service.listarTodos();
+	}
+}
---
+++ CRUDs/Java - Spring/Dia 25/crud2/crud/src/main/java/com/example/crud/infrastructure/entity/Usuario.java
@@ -0,0 +1,42 @@
+package com.example.crud.infrastructure.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name ="usuario")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@NotBlank
+	@Column(name = "nome", nullable = false)
+	private String nome;
+	
+	@NotBlank
+	@Email
+	@Column(name = "email", nullable = false, unique = true)
+	private String email;
+	
+	@NotBlank
+	@Column(name = "senha", nullable = false)
+	private String senha;
+}
