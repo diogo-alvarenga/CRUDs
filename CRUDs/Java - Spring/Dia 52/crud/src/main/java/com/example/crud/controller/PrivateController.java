789f3c1f9a156cd8aa79bc411dc83a1f7bc0a4fb
diogo <alvarwngadiogo228@gmail.com>
2026-02-11 21:55:42 -0300

    commit

---
+++ CRUDs/Java - Spring/Dia 52/crud/src/main/java/com/example/crud/controller/PrivateController.java
@@ -0,0 +1,25 @@
+package com.example.crud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.crud.business.PrivateService;
+
+@RestController
+@RequestMapping("/usuario/private")
+@Validated
+public class PrivateController {
+	
+	@Autowired
+	private PrivateService service;
+	
+	@GetMapping
+	public ResponseEntity<String> usarToken(@RequestHeader(value = "Authorization", required = true)  String authHeader){
+		return ResponseEntity.ok(service.acessar(authHeader));
+	}
+}
